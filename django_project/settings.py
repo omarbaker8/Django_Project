6608559218c2f58516bdbc4d8e5dc523747aae9a
omarbaker8 <128649503+omarbaker8@users.noreply.github.com>
2024-07-04 05:54:42 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_ROOT = BASE_DIR / 'media'
